8fa4cfa6e86a238f54d758806eab200322db2a05
renstillfly <renstillfly2@gmail.com>
2020-11-07 19:34:17 +0800

    try disabling csrf token

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RealRashid\SweetAlert\ToSweetAlert::class,
         ],
@@ -46,23 +46,6 @@ class Kernel extends HttpKernel
         ],
     ];
 
-    // protected $middlewareGroups = [
-    //     'web' => [
-    //         \App\Http\Middleware\EncryptCookies::class,
-    //         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-    //         \Illuminate\Session\Middleware\StartSession::class,
-    //         // \Illuminate\Session\Middleware\AuthenticateSession::class,
-    //         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    //         //\App\Http\Middleware\VerifyCsrfToken::class,
-    //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    //         \App\Http\Middleware\CheckBlocked::class,
-    //     ],
-    //     'api' => [
-    //         //         'throttle:60,1',
-    //         //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    //         //     ],
-    
-    // ];
 
     /**
      * The application's route middleware.
