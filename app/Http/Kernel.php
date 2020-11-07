1d864ab2c95ef5e787af2fad8f0ec3ba4df92c37
renstillfly <renstillfly2@gmail.com>
2020-11-07 18:24:43 +0800

    --allow-empty

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,24 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+    // protected $middlewareGroups = [
+    //     'web' => [
+    //         \App\Http\Middleware\EncryptCookies::class,
+    //         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    //         \Illuminate\Session\Middleware\StartSession::class,
+    //         // \Illuminate\Session\Middleware\AuthenticateSession::class,
+    //         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    //         \App\Http\Middleware\VerifyCsrfToken::class,
+    //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    //         \RealRashid\SweetAlert\ToSweetAlert::class,
+    //     ],
+
+    //     'api' => [
+    //         'throttle:60,1',
+    //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    //     ],
+    // ];
+
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
@@ -35,15 +53,11 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            //\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \RealRashid\SweetAlert\ToSweetAlert::class,
+            \App\Http\Middleware\CheckBlocked::class,
         ],
-
-        'api' => [
-            'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+    
     ];
 
     /**
