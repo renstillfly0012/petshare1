eca8a3019a30ad0f595d1ce23b871f4ffae33a63
renstillfly <renstillfly2@gmail.com>
2020-11-16 09:47:35 +0800

    fixed donation url error with unknown variable v1.1

--- app/Http/Controllers/donationController.php
+++ app/Http/Controllers/donationController.php
@@ -128,8 +128,8 @@ class donationController extends Controller
         // dd($amount);
         $apiContext = new \PayPal\Rest\ApiContext(
             new \PayPal\Auth\OAuthTokenCredential(
-                $sb_client,     // ClientID
-                $sb_secret      // ClientSecret
+                'ATAWA6JeTzh-x6i1c2vr7t4EZYTjAvTMuzuLQB5TG0dlERLTHwsGRRovTO2QtpSDCpiU7cNu3X7NXVwP',     // ClientID
+                'EK97TjY48sf4ho76Si4rMgQQLaLpYzZs6q87zemLd0E7wrjhSFab_s0WAvTi6--iY0cgQDUtWW-J9peE'      // ClientSecret
             )
     );
 
