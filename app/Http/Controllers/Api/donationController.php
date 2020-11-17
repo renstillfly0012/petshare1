7dfd5d38a91b607721c9c7483eb96d98b9f33dc0
renstillfly <renstillfly2@gmail.com>
2020-11-17 23:49:41 +0800

    debugging donation module v1.3

--- app/Http/Controllers/Api/donationController.php
+++ app/Http/Controllers/Api/donationController.php
@@ -22,7 +22,7 @@ class donationController extends Controller
     }
 
     public function storeDonation(DonationRequest $request){
-        // dd($request);
+        dd($request);
         try{
         $donation = Donation::create([
             'donation_name' => $request->donation_name,
