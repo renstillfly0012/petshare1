8de9a959b6d2bef3ad89074b8905a7836a5362cf
renstillfly0012 <201620113@fit.edu.ph>
2020-12-02 01:18:59 +0800

    put role_id

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,6 +72,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => $data['password'],
+            'role_id' => '2',
         ]);
 
         $role = Role::select('id')->where('name', 'foster')->first();
